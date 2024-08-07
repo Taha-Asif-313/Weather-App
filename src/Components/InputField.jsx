11484710f6424f989051c707ae77cbb70255fe49
Tahaiff <tahaasifaqwe@gmail.com>
2024-08-07 03:15:47 -0700

    Updated

--- src/Components/InputField.jsx
+++ src/Components/InputField.jsx
@@ -5,13 +5,13 @@ const InputField = ({ value, onChange, fetchData }) => {
   return (
     <div className="search-field h-[20%] w-full flex items-center justify-center">
       <input
-        className="w-[80%] h-[50%] outline-none rounded-s-md bg-transparent border-2 border-amber-600 px-3 py-2 text-sm"
+        className="w-[80%] h-[55%] outline-none rounded-s-md bg-transparent border-2 border-amber-600 px-3 py-2 text-sm"
         placeholder="Enter city"
         type="text"
         value={value}
         onChange={onChange}
       />
-      <FaSearch onClick={fetchData} className="text-2xl w-[20%] h-[50%] py-2 rounded-e-md text-white cursor-pointer bg-amber-600" />
+      <FaSearch onClick={fetchData} className="text-2xl w-[20%] h-[55%] py-2 rounded-e-md text-white cursor-pointer bg-amber-600" />
     </div>
   );
 };
